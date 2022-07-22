508671e70d68fc9b8b3742199094ab11836b3ffa
saniok333 <alex.o.csc@gmail.com>
2022-07-22 13:25:49 +0300

    Testing the Navigation Flow

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,7 +1,19 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+const SignupScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button
+        title="Go to Signin"
+        onPress={() => navigation.navigate('Signin')}
+      />
+      <Button
+        title="Go to main flow"
+        onPress={() => navigation.navigate('mainFlow')}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,7 +1,15 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackListScreen</Text>;
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <Button
+        title="Go to TrackDetail"
+        onPress={() => navigation.navigate('TrackDetail')}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
