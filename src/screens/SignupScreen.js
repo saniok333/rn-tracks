aa5d6749e2d93e239164de6452ba067c6e12ea10
saniok333 <alex.o.csc@gmail.com>
2022-07-22 14:10:34 +0300

    Created Spacer (helper styling component)

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,20 @@
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Input, Text, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return (
     <View>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button
-        title="Go to Signin"
-        onPress={() => navigation.navigate('Signin')}
-      />
-      <Button
-        title="Go to main flow"
-        onPress={() => navigation.navigate('mainFlow')}
-      />
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
     </View>
   );
 };
