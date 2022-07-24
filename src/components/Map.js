ac0827f4dff3f8cafcb87b71a51e458ce628a3fc
saniok333 <alex.o.csc@gmail.com>
2022-07-24 22:37:59 +0300

    Showing a Map implementation

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,20 @@
 import { StyleSheet, Text } from 'react-native';
+import MapsView from 'react-native-maps';
 
 const Map = () => {
-  return <Text>Map</Text>;
+  return (
+    <MapsView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({ map: { height: 300 } });
 
 export default Map;
