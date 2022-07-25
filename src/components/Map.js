94c407f4c301d1731f25b8d12a3a5547d1c078f2
saniok333 <alex.o.csc@gmail.com>
2022-07-25 16:20:44 +0300

    Live Location Updates implementation

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,33 @@
-import { StyleSheet, Text } from 'react-native';
+import { useContext } from 'react';
+import { StyleSheet, ActivityIndicator } from 'react-native';
 import MapsView, { Polyline } from 'react-native-maps';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-    points.push({
-      latitude: 37.33233 + i * 0.001,
-      longitude: -122.03121 + i * 0.001,
-    });
+  const {
+    state: { currentLocation },
+  } = useContext(LocationContext);
+
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
+
+  const { coords } = currentLocation;
 
   return (
     <MapsView
       style={styles.map}
       initialRegion={{
-        latitude: 37.33233,
-        longitude: -122.03121,
+        ...coords,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    >
-      <Polyline coordinates={points} />
-    </MapsView>
+      region={{
+        ...coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    ></MapsView>
   );
 };
 
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -8,9 +8,11 @@ import {
   Accuracy,
 } from 'expo-location';
 import Map from '../components/Map';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [err, setErr] = useState(null);
 
   const startWatching = async () => {
@@ -26,7 +28,7 @@ const TrackCreateScreen = () => {
           distanceInterval: 10,
         },
         (location) => {
-          console.log(location);
+          addLocation(location);
         }
       );
     } catch (error) {
