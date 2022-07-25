8aa83abdc38780d21d50362459fdcf3d2fe6b251
saniok333 <alex.o.csc@gmail.com>
2022-07-25 14:56:28 +0300

    Reading a location implementation

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,7 +2,11 @@ import '../_mockLocation';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import { requestForegroundPermissionsAsync } from 'expo-location';
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from 'expo-location';
 import Map from '../components/Map';
 import { useEffect, useState } from 'react';
 
@@ -15,6 +19,16 @@ const TrackCreateScreen = () => {
       if (!granted) {
         throw new Error('Location permission not granted');
       }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          console.log(location);
+        }
+      );
     } catch (error) {
       setErr(error);
     }
