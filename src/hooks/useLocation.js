fa343ecdda38d4d25e7b34823043a9b512bc1700
saniok333 <alex.o.csc@gmail.com>
2022-07-25 23:20:35 +0300

    Disabling location tracking implementation

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -5,8 +5,9 @@ import {
 } from 'expo-location';
 import { useEffect, useState } from 'react';
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
+  const [subscriber, setSubscriber] = useState(null);
 
   const startWatching = async () => {
     try {
@@ -14,7 +15,7 @@ export default (callback) => {
       if (!granted) {
         throw new Error('Location permission not granted');
       }
-      await watchPositionAsync(
+      const sub = await watchPositionAsync(
         {
           accuracy: Accuracy.BestForNavigation,
           timeInterval: 1000,
@@ -22,14 +23,20 @@ export default (callback) => {
         },
         callback
       );
+      setSubscriber(sub);
     } catch (error) {
       setErr(error);
     }
   };
 
   useEffect(() => {
-    startWatching();
-  }, []);
+    if (shouldTrack) {
+      startWatching();
+    } else {
+      subscriber.remove();
+      setSubscriber(null);
+    }
+  }, [shouldTrack]);
 
   return [err];
 };
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,16 +1,15 @@
 import '../_mockLocation';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
-import { SafeAreaView } from 'react-navigation';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
 import useLocation from '../hooks/useLocation';
 import { useContext } from 'react';
 import { Context as LocationContext } from '../context/LocationContext';
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
-
+  const [err] = useLocation(isFocused, addLocation);
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h3>Create a Track</Text>
@@ -22,4 +21,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
