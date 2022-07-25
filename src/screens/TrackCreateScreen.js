579b79fec6109da6cdd6495c505eacc0a49da021
saniok333 <alex.o.csc@gmail.com>
2022-07-25 21:47:47 +0300

    Extracting Location Update logic to a hook

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,43 +2,14 @@ import '../_mockLocation';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from 'expo-location';
 import Map from '../components/Map';
-import { useContext, useEffect, useState } from 'react';
+import useLocation from '../hooks/useLocation';
+import { useContext } from 'react';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => {
-          addLocation(location);
-        }
-      );
-    } catch (error) {
-      setErr(error);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
+  const [err] = useLocation(addLocation);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
