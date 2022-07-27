c8c46692c39f794ec7049f8eaa7b990dafc581a0
saniok333 <alex.o.csc@gmail.com>
2022-07-27 15:10:01 +0300

    Track drawing implementation

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,34 +5,32 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
   const {
-    state: { currentLocation },
+    state: { currentLocation, locations },
   } = useContext(LocationContext);
 
   if (!currentLocation) {
     return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
 
-  const { coords } = currentLocation;
-  const initialLocation = {
-    longitude: -122.0312186,
-    latitude: 37.33233141,
-  };
+  const { coords: currentCoords } = currentLocation;
+  const trackCoords = locations.map((location) => location.coords);
 
   return (
     <MapsView
       style={styles.map}
       initialRegion={{
-        ...initialLocation,
+        ...currentCoords,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
     >
       <Circle
-        center={coords}
+        center={currentCoords}
         radius={30}
         strokeColor="rgba(158,158,255, 1.0)"
         fillColor="rgba(158,158,255, 0.3)"
       />
+      <Polyline coordinates={trackCoords} />
     </MapsView>
   );
 };
