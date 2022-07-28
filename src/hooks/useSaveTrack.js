5b807103f56b1cf32fed26549a4c905cb0a198d9
saniok333 <alex.o.csc@gmail.com>
2022-07-28 12:22:58 +0300

    Navigation after reseting to the TrackList implementation

--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -1,6 +1,7 @@
 import { Context as TrackContext } from '../context/TrackContext';
 import { Context as LocationContext } from '../context/LocationContext';
 import { useContext } from 'react';
+import { navigate } from '../navigationRef';
 
 export default () => {
   const { createTrack } = useContext(TrackContext);
@@ -12,6 +13,7 @@ export default () => {
   const saveTrack = () => {
     createTrack(name, locations);
     reset();
+    navigate('TrackList');
   };
 
   return [saveTrack];
