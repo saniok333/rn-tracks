e17b80f69886c445d7ab4c1bdf36e62f7ec719f3
saniok333 <alex.o.csc@gmail.com>
2022-07-28 12:17:03 +0300

    Saving track to the server implementation

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -1,14 +1,19 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case 'create_track':
+      return [...state, action.payload];
     default:
       return state;
   }
 };
 
 const fetchTracks = (dispatch) => () => {};
-const createTrack = (dispatch) => () => {};
+const createTrack = (dispatch) => async (name, locations) => {
+  await trackerApi.post('/tracks', { name, locations });
+};
 
 export const { Provider, Context } = createDataContext(
   trackReducer,
